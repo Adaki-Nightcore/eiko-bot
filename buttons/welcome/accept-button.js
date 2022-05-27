23bd05a93d58a02b8adb941d363c6e63b4cb29ae
Adaki987 <romain0905tahiti@gmail.com>
2022-05-28 00:49:44 +0200

    Eiko Bot

--- buttons/welcome/accept-button.js
+++ buttons/welcome/accept-button.js
@@ -4,11 +4,11 @@ module.exports = {
   name: 'accept-button',
   async runInteraction(client, interaction) {
       let embed = new MessageEmbed()
-      .setTitle('Serveur Adakou:')
+      .setTitle('__Serveur Adakou__:')
       .setDescription(`
         **Merci d'avoir accepté le règlement du serveur Adakou, n'hésite pas a inviter tous tes amis sur notre serveur !
         L'équipe Adakou te souhaite une bonne aventure sur notre serveur.** `)
-      .setImage('https://c.tenor.com/K6hrP0VxZ0AAAAAC/rias-gremory-high-school-dx-d.gif')
+      .setImage('https://c.tenor.com/7gV7MvMk6SkAAAAC/paripi-koumei-eiko-tsukimi.gif')
       await interaction.member.send({embeds: [embed]})
       await interaction.member.roles.add('948740493725024313');
       await interaction.reply({ content: 'Vous avez accepté les règles! Vous pouvez maintenant accéder au serveur, SO COMMENT!', ephemeral: true });
