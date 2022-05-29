a3d9a48a939785189612058f43885aac35fd758c
Adaki987 <romain0905tahiti@gmail.com>
2022-05-29 02:10:38 +0200

    Eiko Bot

--- buttons/welcome/accept-button.js
+++ buttons/welcome/accept-button.js
@@ -8,7 +8,7 @@ module.exports = {
       .setDescription(`
         **Merci d'avoir accepté le règlement du serveur Adakou, n'hésite pas a inviter tous tes amis sur notre serveur !
         L'équipe Adakou te souhaite une bonne aventure sur notre serveur.** `)
-      .setImage('https://c.tenor.com/7gV7MvMk6SkAAAAC/paripi-koumei-eiko-tsukimi.gif')
+      .setImage('https://discord.com/channels/941746802149949541/975585014500720672/980261238556741693')
       await interaction.member.send({embeds: [embed]})
       await interaction.member.roles.add('948740493725024313');
       await interaction.reply({ content: 'Vous avez accepté les règles! Vous pouvez maintenant accéder au serveur, SO COMMENT!', ephemeral: true });
